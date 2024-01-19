c3df49693e84468efea4616e8057dad413d2e422
kopsoyleydi <30636@iitu.edu.kz>
2024-01-19 17:55:59 +0600

    WebSocketConfig update

--- src/main/java/com/example/junior_portal/config/WebSocketConfig.java
+++ src/main/java/com/example/junior_portal/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker( "/user");
+        config.enableSimpleBroker( "/queue");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
