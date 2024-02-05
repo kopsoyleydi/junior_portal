34f54511046c3ceec86c25d64ba978f70a7b15b5
kopsoyleydi <kuralbaibexultan@gmail.com>
2024-02-05 20:17:58 +0600

    Unit tests init

--- src/test/java/com/example/junior_portal/service/ChatServicesTest.java
+++ src/test/java/com/example/junior_portal/service/ChatServicesTest.java
@@ -1,4 +1,20 @@
 package com.example.junior_portal.service;
 
+import com.example.junior_portal.data.repository.ChatMessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK
+)
+@AutoConfigureMockMvc
 public class ChatServicesTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
 }
