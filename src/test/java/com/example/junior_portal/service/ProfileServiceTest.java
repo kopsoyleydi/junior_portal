d7fc1f8de8cbbd1d970e914f57b1df5f7ed27823
kopsoyleydi <kuralbaibexultan@gmail.com>
2024-02-09 22:54:36 +0600

    Unit tests init

--- src/test/java/com/example/junior_portal/service/ProfileServiceTest.java
+++ src/test/java/com/example/junior_portal/service/ProfileServiceTest.java
@@ -1,12 +1,21 @@
 package com.example.junior_portal.service;
 
 
+import com.example.junior_portal.service.portal.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.MOCK
 )
 @AutoConfigureMockMvc
 public class ProfileServiceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProfileService profileService;
 }
