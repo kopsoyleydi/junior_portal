6a05bb2665433185720abda18a22f4622fb9c19e
kopsoyleydi <kuralbaibexultan@gmail.com>
2024-02-10 01:22:50 +0600

    Unit tests init

--- src/test/java/com/example/junior_portal/service/ProfileServiceTest.java
+++ src/test/java/com/example/junior_portal/service/ProfileServiceTest.java
@@ -1,7 +1,11 @@
 package com.example.junior_portal.service;
 
 
+import com.example.junior_portal.data.impl.inter.ProfileRepoInter;
+import com.example.junior_portal.data.mapper.ProfileMapper;
+import com.example.junior_portal.dtos.dto.ProfileDto;
 import com.example.junior_portal.service.portal.ProfileService;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,4 +22,16 @@ public class ProfileServiceTest {
 
     @Autowired
     private ProfileService profileService;
+
+    @Autowired private ProfileRepoInter profileRepoInter;
+
+    @Autowired private ProfileMapper profileMapper;
+
+    @Test
+    public void fillProfileTestSuccess(){
+        ProfileDto profileDto = new ProfileDto();
+        profileDto.setExperience("3 years old");
+        profileDto.setBio("My name is Beksultan");
+        profileDto.setUniversity("IITU");
+    }
 }
