cead0214df348cb6a86b390ed2f36fa0385c04c8
nobeksultan <kuralbaibexultan@gmail.com>
2024-02-29 23:35:06 +0500

    Email Sender Config

--- src/main/java/com/example/junior_portal/util/EmailSender.java
+++ src/main/java/com/example/junior_portal/util/EmailSender.java
@@ -1,8 +1,53 @@
 package com.example.junior_portal.util;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+
 @Component
-public class EmailSender{
+@Slf4j
+public class EmailSender {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+
+    public boolean sendNotificationFromAccept(String to, String subject, String text){
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("kuralbaibexultan.com");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            javaMailSender.send(message);
+            return true;
+        }
+        catch (Exception e){
+            log.info("Exception in sendNotificationFromAccept");
+            e.getMessage();
+            return false;
+        }
+
+    }
+
+    public boolean sendNotificationAboutNews(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("kuralbaibexultan.com");
+            message.setTo(to);
+            message.setSubject("This is from Junior Portal "+  subject);
+            message.setText(text);
+            javaMailSender.send(message);
+            return true;
+        }
+        catch (Exception e){
+            log.info("Exception in sendNotificationFromAccept");
+            e.getMessage();
+            return false;
+        }
+    }
 
 }
