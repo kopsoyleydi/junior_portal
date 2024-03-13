91a79c70ea49c752ede47aa0fb089ad1059c0d73
kopsoyleydi <30636@iitu.edu.kz>
2024-03-13 16:34:20 +0500

    Simple PDF Generator for the Profile CV download

--- src/main/java/com/example/junior_portal/service/documents/PDFGenerator.java
+++ src/main/java/com/example/junior_portal/service/documents/PDFGenerator.java
@@ -1,7 +1,37 @@
 package com.example.junior_portal.service.documents;
 
-import org.springframework.stereotype.Component;
+import com.example.junior_portal.model.Profile;
+import com.example.junior_portal.service.portal.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-@Component
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@Service
+@RequiredArgsConstructor
 public class PDFGenerator {
+
+    private static final String PDF_RESOURCES = "/pdf-resources/";
+    private final ProfileService studentService;
+
+    public File generatePdf(Profile profile) throws IOException {
+        return renderPdf(profile);
+    }
+
+
+    private File renderPdf(Profile profile) throws IOException {
+        File file = File.createTempFile("profile", ".pdf");
+        OutputStream outputStream = new FileOutputStream(file);
+        ITextRenderer renderer = new ITextRenderer(20f * 4f / 3f, 20);
+        renderer.layout();
+        renderer.createPDF(outputStream);
+        outputStream.close();
+        file.deleteOnExit();
+        return file;
+    }
+
 }
